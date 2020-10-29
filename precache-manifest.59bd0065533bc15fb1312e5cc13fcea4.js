b297555748eaa4559b5155d3c9e063c3673c4e9a
hongky <hongkypcx.hp@gmail.com>
2020-10-30 08:30:20 +0900

    Updates

--- precache-manifest.59bd0065533bc15fb1312e5cc13fcea4.js
+++ precache-manifest.59bd0065533bc15fb1312e5cc13fcea4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14ac38861c56059f22760c6fd5b3f6ab",
+    "revision": "ebe97f3054ffcc38846a1fcd67d2c86c",
     "url": "/theseeed/index.html"
   },
   {
-    "revision": "ff2e3c79cbd425a0cb25",
+    "revision": "605b3ecbe2ac43f8652c",
     "url": "/theseeed/static/css/main.ba1f19f6.chunk.css"
   },
   {
-    "revision": "6ad530ddabf8e153c7cc",
-    "url": "/theseeed/static/js/2.5fc21ec0.chunk.js"
+    "revision": "6682744decde3da198b0",
+    "url": "/theseeed/static/js/2.ab8e359c.chunk.js"
   },
   {
-    "revision": "ff2e3c79cbd425a0cb25",
-    "url": "/theseeed/static/js/main.075d930d.chunk.js"
+    "revision": "605b3ecbe2ac43f8652c",
+    "url": "/theseeed/static/js/main.3a2468b5.chunk.js"
   },
   {
     "revision": "f20d619a6eca9ab10eb9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/theseeed/precache-manifest.959e028422c880e4f5d408c4e77baa59.js"
+  "/theseeed/precache-manifest.59bd0065533bc15fb1312e5cc13fcea4.js"
 );
 
 self.addEventListener('message', (event) => {
