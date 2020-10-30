73952fc5665e7eed4f94c8073d7005d3e0ee4aab
hongky <hongkypcx.hp@gmail.com>
2020-10-30 18:41:47 +0900

    Updates

---
+++ precache-manifest.00519eb775595108c70038e7eead55a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f8593c5d80aecfebef8deb1d5c1570",
+    "url": "/theseeed/index.html"
+  },
+  {
+    "revision": "c9fa86f0265db034f875",
+    "url": "/theseeed/static/css/main.f370846c.chunk.css"
+  },
+  {
+    "revision": "962562c863c0cf6ba754",
+    "url": "/theseeed/static/js/2.50b78a6b.chunk.js"
+  },
+  {
+    "revision": "87cac73508bef110e617118860abbdc2",
+    "url": "/theseeed/static/js/2.50b78a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fa86f0265db034f875",
+    "url": "/theseeed/static/js/main.c839939a.chunk.js"
+  },
+  {
+    "revision": "c0f3baaaf9ef31b9dcb2",
+    "url": "/theseeed/static/js/runtime-main.6ff9ab70.js"
+  }
+]);
